43effc30b1d3b269c485f75b1da7ad880c7d343b
Jake <87441812+jakewebd@users.noreply.github.com>
2022-08-31 01:40:35 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 let button = document.querySelectorAll('.show-content');
 let answer = document.querySelectorAll('.answer');
-let question = document.querySelectorAll('.question')
-for (let i = 0; i < button.length; i++) {
+let question = document.querySelectorAll('.question');
+let item = document.querySelectorAll('.accordion-item');
+for (let i = 0; i < item.length; i++) {
     document.body.addEventListener('click', function (e) {
-        if (e.target == button[i]) {
+        if (e.target == item[i] | e.target == button[i] | e.target == question[i] ) {
             answer[i].classList.toggle('active');
             question[i].classList.toggle('qactive');
             button[i].classList.toggle('bactive');
